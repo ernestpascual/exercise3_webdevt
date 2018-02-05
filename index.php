02e39a9a8bd1b04ff95c36df2313e9c41b75b7aa
Ernest Pascual <eepascual@gmail.com>
2018-02-06 00:05:03 +0800

    final commit

--- index.php
+++ index.php
@@ -1,53 +1,45 @@
 <html>
 <head>
 <title>Activity</title>
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">          
 </head>
-<body>
-  <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-    <script type="text/javascript" src="js/materialize.min.js"></script>
+<body class="blue-grey lighten-3">
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 <div class="container">
 	<h1>Select two numbers:</h1>
 	<form method="POST" action="result.php">
 	<div class="row">
 		 <div class="input-field col s3">
           <input placeholder="First Number" name="num1" id="num1" type="text" class="validate">
-        </div>	
+      </div>	
 		<div class="input-field col s3">
           <input placeholder="Second Number" name="num2" id="num2" type="text" class="validate">
-        </div>	
+      </div>	
 	</div>
 	<div class="row">
-		<div class="input-field col s10">
+		<div class="input-field col s6">
 		<h2> Select operation: </h2>
-		<p>
-      <input name="group1" type="radio" id="test1" />
-      <label for="test1">Add</label>
-    </p>
-    <p>
-      <input name="group1" type="radio" id="test2" />
-      <label for="test2">Minus</label>
-    </p>
-    <p>
-      <input class="with-gap" name="group1" type="radio" id="test3"  />
-      <label for="test3">Divide</label>
-    </p>
-    <p>
-      <input name="group1" type="radio" id="test4" />
-      <label for="test4">Multiply</label>
-    </p>
-		</div>
+    <input placeholder="Type add, subtract, multiply or divide" name="oper" id="oper" type="text" class="validate">
+    </div>
 	</div>
 	<div class="row">
-	<div class="input-field col s5">
-	<h2> Enter number: </h2>
-	<input placeholder="From 1-7" name="num3" id="num3" type="text" class="validate">
-	</div></div>
+	  <div class="input-field col s5">
+	    <h2> Enter number: </h2>
+	    <input placeholder="From 1-7" name="num3" id="num3" type="text" class="validate">
+	  </div>
+  </div>
+
+
 	<div class="row">
-	<div class="input-field col s5">
-		<button class="btn waves-effect waves-light  green accent-2" type="submit" name="action">Submit</button>
+	  <div class="input-field col s5">
+		  <button class="btn waves-effect waves-light  grey darken-4	" type="submit" name="action">Submit</button>
+	  </div>
 	</div>
-	</div>
+
+  <select>
+  <option>asa</option>
+
 </form>
 </div>
 </body>
--- result.php
+++ result.php
@@ -7,31 +7,38 @@
   crossorigin="anonymous">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
 </head>
-<body>
+<body class="big purple lighten-1">
 <?php
 /**
 Name is fetched during form submission
 */
 $num1 = $_POST['num1'];
 $num2 = $_POST['num2'];
-$result = (int)$num1 + (int)$num2;
+$oper = strtolower($_POST['oper']);
+$result = 0;
 
-echo "<div class=\"animated bounce infinite center-align\"><h1>" . $result . "</h1></div>";
+if ($oper === "add"): $result = $num1 + $num2;
+elseif($oper === "subtract"): $result = $num1 - $num2;
+elseif($oper === "multiply"): $result = $num1 * $num2;
+elseif($oper === "divide"): $result = $num1 / $num2;
+else: echo "<div class=\"animated bounce infinite center-align big\"><h1>Invalid operation! </h1></div>";
+endif;
+
+echo "<div class=\"animated bounce infinite center-align big\"><h1>" . $result . "</h1></div>";
 
 $day = $_POST['num3'];
 
 switch($day):
-case "1": echo "<div class=\"animated tada infinite center-align\"><h1>Monday</h1></div>"; break;
-case "2": echo "<div class=\"animated tada infinite center-align\"><h1>Tuesday</h1></div>"; break;
-case "3": echo "<div class=\"animated tada infinite center-align\"><h1>Wednesday</h1></div>"; break;
-case "4": echo "<div class=\"animated tada infinite center-align\"><h1>Thursday</h1></div>"; break;
-case "5": echo "<div class=\"animated tada infinite center-align\"><h1>Friday</h1></div>"; break;
-case "6": echo "<div class=\"animated tada infinite center-align\"><h1>Saturday</h1></div>"; break;
-case "7": echo "<div class=\"animated tada infinite center-align\"><h1>Sunday</h1></div>"; break;
-default: echo "<div class=\"animated  tada infinite center-align\"><h1>Invalid Number!</h1></div>"; break;
+case "1": echo "<div class=\"animated tada infinite center-align big\"><h1>Monday</h1></div>"; break;
+case "2": echo "<div class=\"animated tada infinite center-align big\"><h1>Tuesday</h1></div>"; break;
+case "3": echo "<div class=\"animated tada infinite center-align big\"><h1>Wednesday</h1></div>"; break;
+case "4": echo "<div class=\"animated tada infinite center-align big\"><h1>Thursday</h1></div>"; break;
+case "5": echo "<div class=\"animated tada infinite center-align big\"><h1>Friday</h1></div>"; break;
+case "6": echo "<div class=\"animated tada infinite center-align big\"><h1>Saturday</h1></div>"; break;
+case "7": echo "<div class=\"animated tada infinite center-align big\"><h1>Sunday</h1></div>"; break;
+default: echo "<div class=\"animated  tada infinite center-align big\"><h1>Invalid Number!</h1></div>"; break;
 endswitch;
 
 ?>
-
 </body>
 </html>
